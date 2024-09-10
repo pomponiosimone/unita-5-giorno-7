858aaf25b0096a065d3d73ce0ae72e4bdfd0175c
Simone Pomponio <Pomponio671@gmail.com>
2024-09-10 14:49:00 +0200

    Initial commit

---
+++ src/main/java/pomponiosimone/unita_giorno_7/UnitaGiorno7Application.java
@@ -0,0 +1,13 @@
+package pomponiosimone.unita_giorno_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitaGiorno7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitaGiorno7Application.class, args);
+	}
+
+}
